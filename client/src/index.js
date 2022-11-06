85178a9c6527afe953ddbf275ad2e3bed7b9d34f
shang8024 <jiale.shang@mail.utoronto.ca>
2022-11-06 14:51:52 -0500

    put SENTRY_DSN in env

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,6 @@ import { BrowserTracing } from "@sentry/tracing";
 import App from './App';
 
 Sentry.init({
-  dsn: "https://ceea19d7e42b4584aae7d11eb3f2ddcd@o358880.ingest.sentry.io/4504109347831808",
   integrations: [new BrowserTracing()],
 
   // Set tracesSampleRate to 1.0 to capture 100%

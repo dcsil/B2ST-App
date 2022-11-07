d0a719ce8d2f9dd669a3b1ed681d9cc1f2d20700
shang8024 <jiale.shang@mail.utoronto.ca>
2022-11-07 16:30:37 -0500

    fix local db connection

--- server/config/db.js
+++ server/config/db.js
@@ -2,11 +2,10 @@ const mongoose = require("mongoose");
 require("dotenv").config({path: "./.env"});
 const print = console.log
 
-const db = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER_NAME}.sgggldd.mongodb.net/${process.env.DB_DATABASE}?retryWrites=true&w=majority`;
+const db = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_DATABASE}?retryWrites=true&w=majority`;
 
 const connectDB = async() =>{
     try{
-        print(db);
         await mongoose.connect(db,{
             useNewUrlParser: true
         });

b2c0684038493a887903cc9b8a5678c6eb816c23
FrankQQQQ <18518553618@163.com>
2022-11-07 17:46:13 -0500

    Testing, service.yml reformat

--- client/tests/react-init-test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
-import App from "../src/App"
-
-test("Render Correctly", () =>{
-    const tree = renderer.create(<App></App>).toJSON();
-    expect(tree).toMatchSnapshot();
-})

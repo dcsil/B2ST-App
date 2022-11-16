7067a090b0d824239cd830cc1537860533c53861
shang8024 <jiale.shang@mail.utoronto.ca>
2022-11-16 17:00:19 -0500

    get SMS

--- server/Routes/sms.js
+++ server/Routes/sms.js
@@ -3,6 +3,8 @@ const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const phone = process.env.PHONE_NUMBER;
 const client = require('twilio')(accountSid, authToken);
+const { MessagingResponse } = require('twilio').twiml;
+
 const sendSMS = (mes, to) => {
     client.messages
     .create({
@@ -13,4 +15,11 @@ const sendSMS = (mes, to) => {
     .then(message => console.log(message.sid));
 }
 
-module.exports = sendSMS;
+const getSMS = async (datesent,limit) => {
+  return await client.messages.list({
+        dateSent: datesent,
+        limit:limit
+    });
+}
+
+module.exports = {sendSMS, getSMS};

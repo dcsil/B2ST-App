ebad1b53caa429503e7efd9df9f363f30f58539e
FrankQQQQ <18518553618@163.com>
2022-11-21 18:59:41 -0500

    add  context

---
+++ client/src/context/AuthContext.js
@@ -0,0 +1,35 @@
+import { createContext, useReducer, useEffect } from "react"
+export const AuthContext = createContext()
+
+export const authReducer = (state, action) =>{
+    switch (action.type) {
+        case "LOGIN":
+            return { user: action.payload }
+        case "LOGOUT":
+            return {user: null}
+        default:
+            return state
+    }
+}
+
+export const AuthContextProvider = ({children})=>{
+    const [state, dispatch] = useReducer(authReducer, {
+        user: null,
+
+    })
+
+    // if refresh, check if the user is signin already.
+    useEffect(()=>{
+        const user = JSON.parse(localStorage.getItem("user"))
+        if (user){
+            dispatch({type:"LOGIN", payload: user})
+        }
+    },[])
+
+    console.log("Auth Context state: ", state)
+
+    return (
+        <AuthContext.Provider value = {{...state, dispatch}}>{ children }</AuthContext.Provider>
+    )
+}
+

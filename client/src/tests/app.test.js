88991e33baf9c70ddd3ea6164dc94b3091b6a8ab
FrankQQQQ <18518553618@163.com>
2022-11-22 18:49:47 -0500

    remove testcase since error ReactDOM.render is no longer supported in React 18. Use createRoot instead

--- client/src/tests/app.test.js
+++ client/src/tests/app.test.js
@@ -3,9 +3,9 @@ import ReactDOM from 'react-dom';
 import App from '../App';
 import { render, screen } from '@testing-library/react';
 
-//@babel/preset-react
-//@babel/plugin-syntax-jsx
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+// //@babel/preset-react
+// //@babel/plugin-syntax-jsx
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+// });

fbaf9aa22cc1f8e671731937a98239d0a2aeee05
FrankQQQQ <18518553618@163.com>
2022-11-25 12:56:18 -0500

    update new testcase, fix error: useContext should be inside context component

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,15 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
 
-jest.mock("react-dom", () => ({ render: jest.fn() }));
-
-describe("Application root", () => {
-  it("should render without crashing", () => {
-    const div = document.createElement("div");
-    div.id = "root";
-    document.body.appendChild(div);
-    require("./index.js");
-    expect(ReactDOM.render).toHaveBeenCalledWith(<App />, div);
-  });
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../App';
+import { AuthContextProvider } from "../context/AuthContext"
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>, div);
 });

54793643d0f237b5966b39e4ac68ad16e9f42d20
FrankQQQQ <18518553618@163.com>
2022-11-25 12:34:16 -0500

    change frontend test remove console error console.error       Warning: ReactDOM.render is no longer supported in React 18.

--- client/src/tests/app.test.js
+++ client/src/tests/app.test.js
@@ -5,7 +5,16 @@ import { render, screen } from '@testing-library/react';
 
 // //@babel/preset-react
 // //@babel/plugin-syntax-jsx
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+// });
+
+const sum = (a, b) =>{
+  return a + b
+}
+
+it('sums numbers', () => {
+  expect(sum(1, 2)).toEqual(3);
+  expect(sum(2, 2)).toEqual(4);
 });

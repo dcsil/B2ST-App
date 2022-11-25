77e79e2f8936630ed8c89b7ea60962e3d90ce7b5
FrankQQQQ <18518553618@163.com>
2022-11-25 12:53:37 -0500

    fix usecontext should be call inside context component error

--- client/src/tests/app.test.js
+++ client/src/tests/app.test.js
@@ -1,20 +1,12 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
-import { render, screen } from '@testing-library/react';
-
-// //@babel/preset-react
-// //@babel/plugin-syntax-jsx
-// it('renders without crashing', () => {
-//   const div = document.createElement('div');
-//   ReactDOM.render(<App />, div);
-// });
-
-const sum = (a, b) =>{
-  return a + b
-}
-
-it('sums numbers', () => {
-  expect(sum(1, 2)).toEqual(3);
-  expect(sum(2, 2)).toEqual(4);
+import { AuthContextProvider } from "../context/AuthContext"
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>, div);
 });

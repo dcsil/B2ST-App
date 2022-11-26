bcedd53532b90234e1e7106ef11a5eddc72b8684
shang8024 <jiale.shang@mail.utoronto.ca>
2022-11-25 23:19:08 -0500

    fix react jest config

--- client/src/tests/app.test.js
+++ client/src/tests/app.test.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../App';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 //@babel/preset-react
 //@babel/plugin-syntax-jsx
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  render(<App />);
 });

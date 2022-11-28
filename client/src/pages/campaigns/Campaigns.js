3bbf87cb503e7f58193132238b495a8c893f1383
shang8024 <jiale.shang@mail.utoronto.ca>
2022-11-28 15:22:14 -0500

    add campaign monitoring order chart

---
+++ client/src/pages/campaigns/Campaigns.js
@@ -0,0 +1,104 @@
+import * as React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import DashboardAppBar from '../dashboard/DashboardAppBar';
+import { Container,Grid,Toolbar,Box,Paper} from '@mui/material';
+import AppChart from '../../sections/AppChart';
+
+const mdTheme = createTheme();
+const api_url = process.env.NODE_ENV === "production" ? process.env.REACT_APP_HEROKU_HOST : process.env.REACT_APP_API_URL;
+
+const cur_date=new Date();
+const date=new Date();
+date.setFullYear(cur_date.getFullYear()-1);
+const ordersLabels = [date.toLocaleString('en', { month: 'numeric', year: 'numeric',day:'numeric'})];
+while (date.setMonth(date.getMonth() + 1) < cur_date) {
+    ordersLabels.unshift(date.toLocaleString('en', { month: 'numeric', year: 'numeric', day:'numeric' }));
+}
+console.log(ordersLabels);
+
+const ordersData=[
+    {
+        name: 'Item A',
+        type: 'line',
+        fill: 'solid',
+        data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
+    },
+    {
+        name: 'Item B',
+        type: 'line',
+        fill: 'solid',
+        data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
+    },
+    {
+        name: 'Item C',
+        type: 'line',
+        fill: 'solid',
+        data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
+    },
+];
+
+
+function CampaignsContent() {
+  const [open, setOpen] = React.useState(false);
+  const [selected, setSelected] = React.useState([]);
+  const [success, setSuccess] = React.useState(false);
+
+  return (
+    <ThemeProvider theme={mdTheme}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <DashboardAppBar name="Campaigns"/>
+        <Box
+          component="main"
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[100]
+                : theme.palette.grey[900],
+            flexGrow: 1,
+            height: '100vh',
+            overflow: 'auto',
+          }}
+        >
+          <Toolbar />
+          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+          <Grid container spacing={3}>
+              <Grid item xs={12}>
+                <AppChart title='Orders' subheader='in last year' chartLabels={ordersLabels} chartData={ordersData} />
+              </Grid>
+              {/* Chart */}
+              <Grid item xs={12} md={6} lg={6}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: 240,
+                  }}
+                >
+                </Paper>
+              </Grid>
+              {/* Recent Deposits */}
+              <Grid item xs={12} md={6} lg={6}>
+                <Paper
+                  sx={{
+                    p: 2,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: 240,
+                  }}
+                >
+                </Paper>
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
+      </Box>
+    </ThemeProvider>
+  );
+}
+
+export default function Campaigns() {
+  return <CampaignsContent />;
+}

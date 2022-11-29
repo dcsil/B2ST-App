6f7cf3e1d2a204aa1c5ac9a3fe4d7526c6869d97
FrankQQQQ <18518553618@163.com>
2022-11-28 20:06:53 -0500

    post purchase request to server

--- server/routes/plans.js
+++ server/routes/plans.js
@@ -7,18 +7,39 @@ const print = console.log
 const Stripe = require('stripe');
 const stripe = Stripe(process.env.STRIPE_SET);
 
-router.get("/", async (req, res) =>{
+router.get("/price", async (req, res) =>{
     const prices = await stripe.prices.list({
         apiKey: process.env.STRIPE_SEC
     })
-
-    
       res.json(prices)
 
 })
 
 
 
+router.post("/session", async (req, res) =>{
+    const {email} = req.body
+    const user = await User.findOne({email: email})
+    // res.send(user)
+    // return
+
+    const session = await stripe.checkout.sessions.create({
+        mode: "subscription",
+        payment_method_types:["card"],
+        line_items: [
+            {
+                price: "price_1M98fxIlyH1Yo9HOTNgKUL8c", // this can be changed to another hashed price
+                quantity: 1
+            }
+        ],
+        success_url: "http://localhost:3000/",
+        cancel_url: "http://baidu.com",  //need change later!
+        customer: user.customer.id
+    }, {
+        apiKey: process.env.STRIPE_SEC
+    })
+    return res.json({user: user, price: session})
+})
 
 
 

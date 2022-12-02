9faec91f2f00136d90df4c6558a88c04214fcb40
shang8024 <jiale.shang@mail.utoronto.ca>
2022-12-02 02:27:21 -0500

    refactor plan page, remove pricing

--- client/src/pages/Plan.js
+++ client/src/pages/Plan.js
@@ -1,20 +1,15 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardHeader from '@mui/material/CardHeader';
 import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import StarIcon from '@mui/icons-material/StarBorder';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import { Link } from "react-router-dom";
 import LandingAppBar from './LandingAppBar';
-import {useState} from "react"
 import { useAuthContext } from '../hooks/useAuthContext';
 import axios from 'axios';
 import "../App.css"
@@ -52,32 +47,73 @@ const tiers = [
         'Email support',
         'Free installation',
     ],
-  },
-
-  
+  }
 ];
+
+const PlanCard = (props) => {
+  const {tier, index, checkout} = props;
+  return (
+    <Grid item key={index} xs={12} sm={6} md={4}>
+      <Card onClick={(e)=>{checkout(e, {index})}} className='payment_card' >
+        <CardHeader
+          title={tier.title}
+          subheader={tier.subheader}
+          titleTypographyProps={{ align: 'center' }}
+          action={tier.title === 'Pro' ? <StarIcon /> : null}
+          subheaderTypographyProps={{
+            align: 'center',
+          }}
+          sx={{
+            backgroundColor: (theme) =>
+              theme.palette.mode === 'light'
+                ? theme.palette.grey[200]
+                : theme.palette.grey[700],
+          }}
+        />
+        <CardContent>
+          <Box
+            sx={{
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'baseline',
+              mb: 2,
+            }}
+          >
+            <Typography component="h2" variant="h3" color="text.primary">
+              ${tier.price}
+            </Typography>
+            <Typography variant="h6" color="text.secondary">
+              /mo
+            </Typography>
+          </Box>
+          <ul>
+            {tier.description.map((line) => (
+              <Typography
+                component="li"
+                variant="subtitle1"
+                align="center"
+                key={line}
+              >
+                {line}
+              </Typography>
+            ))}
+          </ul>
+        </CardContent>
+      </Card>
+    </Grid>
+  );
+}
 
 const Plan = () =>{
     const {user} = useAuthContext()
     const checkout = async (e,index)=>{
       e.preventDefault()
-      
-        print(index)
-        const email = (user.email? user.email: user.user.email)
-        // if(parseInt(index.index) === 0){
-        //   print (0)
-        // }
-        // else if(parseInt(index.index) === 1){
-        //   print(1)
-        // }
-        // else{
-        //   print(2)
-        // }
-        const {data: response} = await axios.post("http://localhost:5000/plans/session", {email: email, plan: index.index})
-        print(response)
-        window.location.href = response.price.url
+      print(index)
+      const email = (user.email? user.email: user.user.email)
+      const {data: response} = await axios.post("http://localhost:5000/plans/session", {email: email, plan: index.index})
+      print(response)
+      window.location.href = response.price.url
     }
-
 
   return ( 
     <ThemeProvider theme={theme}>
@@ -96,72 +132,11 @@ const Plan = () =>{
         <Typography variant="h5" align="center" color="text.secondary" component="p">
           Subscribe to our website to use our service!
         </Typography>
-        {/* <Button sx={{margin:1}} variant="outlined">
-            Start free trial for 14 days
-        </Button> */}
       </Container>
-      {/* End hero unit */}
       <Container maxWidth="md" component="main">
         <Grid container spacing={5} alignItems="flex-end">
           {tiers.map((tier, index) => (
-            // Enterprise card is full width at sm breakpoint
-            
-            <Grid key = {index} className='payment_card'
-            
-              // item
-              // key={tier.title}
-              // xs={12}
-              // sm={tier.title === 'Enterprise' ? 12 : 6}
-              // md={4}
-            >
-              <Card  onClick={(e)=>{checkout(e, {index})}}>
-              
-                <CardHeader
-                  title={tier.title}
-                  subheader={tier.subheader}
-                  titleTypographyProps={{ align: 'center' }}
-                  action={tier.title === 'Pro' ? <StarIcon /> : null}
-                  subheaderTypographyProps={{
-                    align: 'center',
-                  }}
-                  sx={{
-                    backgroundColor: (theme) =>
-                      theme.palette.mode === 'light'
-                        ? theme.palette.grey[200]
-                        : theme.palette.grey[700],
-                  }}
-                />
-                <CardContent>
-                  <Box
-                    sx={{
-                      display: 'flex',
-                      justifyContent: 'center',
-                      alignItems: 'baseline',
-                      mb: 2,
-                    }}
-                  >
-                    <Typography component="h2" variant="h3" color="text.primary">
-                      ${tier.price}
-                    </Typography>
-                    <Typography variant="h6" color="text.secondary">
-                      /mo
-                    </Typography>
-                  </Box>
-                  <ul>
-                    {tier.description.map((line) => (
-                      <Typography
-                        component="li"
-                        variant="subtitle1"
-                        align="center"
-                        key={line}
-                      >
-                        {line}
-                      </Typography>
-                    ))}
-                  </ul>
-                </CardContent>
-              </Card>
-            </Grid>
+            <PlanCard tier={tier} index={index} checkout={checkout}/>
           ))}
         </Grid>
       </Container>

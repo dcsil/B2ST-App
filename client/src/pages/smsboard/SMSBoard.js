284567bb669cb0ee44afd978da1d83d27dc3ad04
shang8024 <jiale.shang@mail.utoronto.ca>
2022-12-02 04:15:37 -0500

    refactor SMSBoard, moving to hooks

--- client/src/pages/smsboard/SMSBoard.js
+++ client/src/pages/smsboard/SMSBoard.js
@@ -6,62 +6,17 @@ import SMSTable from './SMSOverview';
 import { useAuthContext } from '../../hooks/useAuthContext';
 import ContactDialog from './ContactDialog';
 import DashboardPageProvider from '../../components/DashboardPageProvider';
-const api_url = process.env.NODE_ENV === "production" ? process.env.REACT_APP_HEROKU_HOST : process.env.REACT_APP_API_URL;
+import { useSendText } from '../../hooks/useSendText';
+import { useAddContact } from '../../hooks/useAddContact';
 
-function SMSBoardContent() {
+export default function SMSBoard() {
   const [open, setOpen] = React.useState(false);
   const [contactOpen, setContactOpen] = React.useState(false);
   const [selected, setSelected] = React.useState([]);
   const [alert, setAlert] = React.useState({severity:'',message:''});
   const {user} = useAuthContext();
-  const sendText = async (text,time,code) => {
-    const email=(user.email? user.email: user.user.email);
-    const request = new Request(`${api_url}/sms/sendAll`, {
-      method: 'post',
-      body: JSON.stringify({ mes:text, to:selected, sendAt:time,user:email, hasCode:code }),
-      headers: {
-        Accept: 'application/json, text/plain, */*',
-        'Content-Type': 'application/json',
-      },
-    });
-    fetch(request)
-      .then(res => {
-        if (res.status === 200) {
-          setAlert({severity:'success',message:'Texts sent successfully!'});
-        } else {
-          setAlert({severity:'error',message:'Error sending texts!'});
-          console.log(res);
-        }
-      })
-      .catch(error => {
-        setAlert({severity:'error',message:'Server Error!'});
-        console.log(error);
-      });
-  }
-  const addContact = async (name,phone) => {
-    const email=(user.email? user.email: user.user.email);
-    const request = new Request(`${api_url}/contact/add`, {
-      method: 'post',
-      body: JSON.stringify({ name:name, phone:phone, user:email }),
-      headers: {
-        Accept: 'application/json, text/plain, */*',
-        'Content-Type': 'application/json',
-      },
-    });
-    fetch(request)
-      .then(res => {
-        if (res.status === 200) {
-          setAlert({severity:'success',message:'Contact added successfully!'});
-        } else {
-          setAlert({severity:'error',message:'Error adding contact!'});
-          console.log(res);
-        }
-      })
-      .catch(error => {
-        setAlert({severity:'error',message:'Server Error!'});
-        console.log(error);
-      });
-  }
+  const {sendText} = useSendText(user,setAlert);
+  const {addContact} = useAddContact(user,setAlert);
 
   return (
     <DashboardPageProvider name="SMS Board" alert={alert} setAlert={setAlert}>
@@ -81,7 +36,7 @@ function SMSBoardContent() {
       <TextDialog
         open={open}
         closeDialog={() => setOpen(false)}
-        sendText={(text,time,code) => sendText(text,time,code)}
+        sendText={(text,time,code) => sendText(text,selected,time,code)}
       />
       <ContactDialog
         open={contactOpen}
@@ -90,8 +45,4 @@ function SMSBoardContent() {
       />
     </DashboardPageProvider>
   );
-}
-
-export default function SMSBoard() {
-  return <SMSBoardContent />;
 }

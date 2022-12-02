e17a2dcf8ced44302b38ac055827801371d7cbd7
bhatiate <tevan.bhatia@mail.utoronto.ca>
2022-12-02 17:57:09 -0500

    Refactoring Auth Context handling in hooks

--- client/src/hooks/requests/communication.js
+++ client/src/hooks/requests/communication.js
@@ -19,5 +19,20 @@ function handleRequest(request, setAlert, type) {
       console.log(error);
     });
 }
+async function handleAuthContext(setIsLoading, setError, dispatch, response, authType) {
+    const json = await response.json()
+    if (!response.ok){
+        setIsLoading(false)
+        setError(json.error)
+    }
+    else{
+        // save the user to local storage
+        localStorage.setItem("user", JSON.stringify(json))
 
-module.exports = { handleRequest };
+        //update auth context
+        dispatch({type: authType, payload: json})
+        setIsLoading(false)
+    }
+}
+
+module.exports = { handleRequest, handleAuthContext };

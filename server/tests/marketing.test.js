b40f8fb7414d3772180a0c2a69a3dbc36575fe4a
bhatiate <tevan.bhatia@mail.utoronto.ca>
2022-12-06 02:34:16 -0500

    More marketing tests

--- server/tests/marketing.test.js
+++ server/tests/marketing.test.js
@@ -18,9 +18,46 @@ afterAll(async () => {
     await db.close();
 });
 
+describe("Test the ML model initiation", () => {
+    test("Analyze function should run correctly", async () => {
+        let script = app.get("script");
+        expect(script).not.toBe(null);
+    }); 
+});
+
 describe("POST /marketing/query", () => {
     it("should return marketing prediction", async () => {
         const res = await request(app).post("/marketing/query").type('json').send({query: [[
+            "2388440981134609919",
+            "2273948312304353947",
+            "smartphone",
+            "huawei"
+        ]]});
+        expect(res.statusCode).toBe(200);
+    });
+    it("should return consecutive predictions", async () => {
+        const res = await request(app).post("/marketing/query").type('json').send({query: [[    
+            "2388440981134609919",
+            "2273948312304353947",
+            "smartphone",
+            "huawei"
+        ]]});
+        expect(res.statusCode).toBe(200);
+        const res2 = await request(app).post("/marketing/query").type('json').send({query: [[
+            "2388440981134609919",
+            "2273948312304353947",
+            "smartphone",
+            "huawei"
+        ]]});
+        expect(res2.statusCode).toBe(200);
+    });
+    it("should return multiple predictions", async () => {
+        const res = await request(app).post("/marketing/query").type('json').send({query: [[
+            "2388440981134609919",
+            "2273948312304353947",
+            "smartphone",
+            "huawei"
+        ], [
             "2388440981134609919",
             "2273948312304353947",
             "smartphone",

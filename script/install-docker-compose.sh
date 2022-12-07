9e9e62ab297b95edf03fa9b05d09634790885221
bhatiate <tevan.bhatia@mail.utoronto.ca>
2022-12-07 01:37:28 -0500

    Docker compose installation and insertion into bootstrap script

---
+++ script/install-docker-compose.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -SL https://github.com/docker/compose/releases/download/v2.13.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+chmod +rwx /usr/local/bin/docker-compose
+echo "Docker Compose installed successfully"

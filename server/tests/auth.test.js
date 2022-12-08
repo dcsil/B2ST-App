53c4c58d0cdafda6cf72cfb9cf2afa1dfb4c0d07
FrankQQQQ <18518553618@163.com>
2022-12-08 14:24:59 -0500

    test

--- server/tests/auth.test.js
+++ server/tests/auth.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const express = require("express");
 require("dotenv").config({ path: "./.env" });
 const { MongoClient } = require("mongodb");
-
+const print = console.log
 const app = express();
 app.use(express.json());
 
@@ -21,9 +21,46 @@ beforeAll(async () => {
 
 
 describe("GET /plans/price", () => {
-    it("should get current plan list", async () => {
+    it("should not have error status", async () => {
       const res = await request(app).get("/plans/price");
         expect(res).not.toBe(null);
-        // expect(res.status).toBe(200);
+        expect(res).not.toBe(404)
     });
+
+    it("should get current plan list", async () => {
+        const res = await request(app).get("/plans/price");
+          expect(res.body).not.toBe(null);
+          expect(res.statusCode).not.toBe(400);
+      });
 });
+
+describe("GET /plans/price", () => {
+    it("should not have error status", async () => {
+      const res = await request(app).get("/plans/price");
+        expect(res).not.toBe(null);
+        expect(res).not.toBe(404)
+    });
+
+    it("should get current plan list", async () => {
+        const res = await request(app).get("/plans/price");
+          expect(res.body).not.toBe(null);
+          expect(res.statusCode).not.toBe(400);
+      });
+});
+
+
+describe("POST /subs", () => {
+    it("should get the plan of the sended user", async () => {
+      const res = await request(app)
+        .post("/subs")
+        .type("json")
+        .send({
+            "email": "a@a.com"
+        });
+    // print("+++++++++++++++++++++++++++++++++++++++++")
+    // print(res.body)
+      expect(res.statusCode).not.toBe(400);
+    });
+    
+  });
+  
